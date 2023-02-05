bd2cbf8b6da1433225271a728fb4a22d4a552db2
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-02-05 14:48:12 +0300

    configure useDocumentTitle custom hook.

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,15 +2,18 @@ import { useEffect } from "react";
 
 /* ✅ create a new function called useDocumentTitle */
 /* 👀 export function useDocumentTitle() {} */
+export function useDocumentTitle(input) {
+  useEffect(() => {
+    document.title = input;
+  }, [input]);
+}
 
 export default function Home() {
   /* 
     ✅ move the useEffect code into your useDocumentTitle function
    then, call the useDocumentTitle hook in your component
   */
-  useEffect(() => {
-    document.title = "Welcome to the home page!";
-  }, []);
+  useDocumentTitle("wassuuuhh");
 
   return (
     <div>
